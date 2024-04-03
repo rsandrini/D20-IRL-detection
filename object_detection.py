24046db9f2fd7b97080149a9f94ec6d3d1b3e405
Rafael Sandrini <rsandrini@forte.io>
2024-04-03 16:02:33 -0300

    Fix objects box detection

--- object_detection.py
+++ object_detection.py
@@ -159,7 +159,7 @@ class ObjectDetector:
             # cv2.rectangle(image, (xmin, label_ymin - label_size[1] - 10),
             #               (xmin + label_size[0], label_ymin + 5), (255, 255, 255), cv2.FILLED)
             cv2.rectangle(image,
-                          (box_data.label_x, box_data.label_y - box_data.label_width - 10),
+                          (box_data.label_x, box_data.label_y - box_data.label_height - 10),
                           (box_data.label_x + box_data.label_width, box_data.label_y + box_data.label_height + 5),
                           (255, 255, 255),
                           cv2.FILLED)
